f87b387414501f7b79d3119d313187a2dccb2733
Richard Carter Colina <ricarcol.rc@gmail.com>
2025-04-05 14:43:09 -0600

    Some light refactoring
    Split code into multiple files and made the chunk_add/subtract function
    signatures more consistent with each other.

---
+++ src/temp_buffer.c
@@ -0,0 +1,40 @@
+#include "temp_buffer.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+
+uint8_t temp_buffer[TEMP_BUFFER_CAPACITY];
+size_t tmp_buffer_count = 0;
+size_t tmp_scratch_pos = 0;
+bool tmp_modifying_scratch = false;
+
+void *tmp_push(void *data, size_t data_size) {
+	if(tmp_buffer_count + data_size >= TEMP_BUFFER_CAPACITY) {
+		fprintf(stderr, "[ERROR]: Temporary buffer capacity exceeded!");
+		return NULL;
+	}
+	memcpy(temp_buffer + tmp_buffer_count, data, data_size);
+	void *ret = temp_buffer + tmp_buffer_count;
+	tmp_buffer_count += data_size;
+	return ret;
+}
+
+void tmp_free(void) {
+	tmp_buffer_count = 0;
+}
+
+void *tmp_start_scratch(void) {
+    assert(tmp_modifying_scratch == false && "[ERROR]: Cannot begin a scratch session without ending the previous session first.");
+	tmp_scratch_pos = tmp_buffer_count;
+    tmp_modifying_scratch = true;
+	return (void *)(temp_buffer + tmp_scratch_pos);
+}
+
+size_t tmp_end_scratch(void) {
+    assert(tmp_modifying_scratch == true && "[ERROR]: Cannot end a scratch session without starting a previous session first.");
+	size_t scratch_size = tmp_buffer_count - tmp_scratch_pos;
+	tmp_buffer_count = tmp_scratch_pos;
+    tmp_modifying_scratch = false;
+	return scratch_size;
+}
--- windows.mak
+++ windows.mak
@@ -3,7 +3,7 @@ all: mrat.exe
 
 mrat.exe:
 	if not exist .\bin\ mkdir .\bin\ ;
-	clang -o ./bin/mrat.exe -std=c11 -Wall -pedantic ./src/main.c
+	clang -o ./bin/mrat.exe -std=c11 -Wall -pedantic -I./src/ ./src/main.c ./src/integer.c ./src/temp_buffer.c
 
 clean:
 	rmdir /S ./bin\
